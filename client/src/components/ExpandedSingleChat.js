a89d8bd1e3c5e6cb5be024df8d50dad60447259a
tunde <icepaigez@gmail.com>
2020-12-31 08:15:50 +0100

    styling the expanded chat area

--- client/src/components/ExpandedSingleChat.js
+++ client/src/components/ExpandedSingleChat.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVert from "@material-ui/icons/MoreVert";
 
 import "./styles/chat.css";
 
@@ -38,8 +41,27 @@ class ExpandedSingleChat extends Component {
 			<div className="singlechat"> 
 				<div className="chat__header"> 
 					<Avatar />
+					<div className="chat__headerInfo"> 
+						<h3>Customer Name</h3>
+						<p>Last seen at ...</p>
+					</div>
+					<div className="chat__headerRight"> 
+						<IconButton>
+							<SearchOutlined />
+						</IconButton>
+						<IconButton>
+							<AttachFile />
+						</IconButton>
+						<IconButton>
+							<MoreVert />
+						</IconButton>
+					</div>
 			    </div>
 			    <div className="chat__body"> 
+			    	<p className={`chat__message ${true && "chat__receiver"}`}>
+			    		Hey Guys
+			    		<span className="chat__timestamp">3:52pm</span> 
+			    	</p>
 			    </div>
 			    <div className="chat__footer"> 
 			    </div>
