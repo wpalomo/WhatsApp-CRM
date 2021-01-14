bed4ef9e97f22883981eac09557a33404abc91a4
tunde <icepaigez@gmail.com>
2021-01-14 18:48:36 +0100

    styled the admin menu bar

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 import "../styles/admin.css";
 
 const Admin = () => {
@@ -17,6 +18,7 @@ const Admin = () => {
 			<div className="admin__parent">
 				<Navbar sidebarOpen={sidebarOpen} openSideBar={openSideBar}/>
 				<h1>Admin Dashboard</h1>
+				<Sidebar sidebarOpen={sidebarOpen} closeSideBar={closeSideBar}/>
 			</div>
 		</div>
 	)
---
+++ client/src/components/Admin/Sidebar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./styles/sidebar.css";
+
+const Sidebar = ({ sidebarOpen, closeSideBar }) => {
+	return(
+		<div id="sidebar" className={sidebarOpen ? "sidebar_responsive" : ""}>
+			<div className="sidebar__title">
+				<div className="sidebar__img">
+					<img src=""/>
+					<h1>Company Name</h1>
+				</div>
+				<i className="fa fa-times" id="sidebarIcon" onClick={closeSideBar}></i>
+			</div>
+			<div className="sidebar__menu">
+				<div className="sidebar__link active_menu_link">
+					<i className="fa fa-home"></i>
+					<a href="#">Dashboard</a>
+				</div>
+				<h2>MNG</h2>
+				<div className="sidebar__link">
+					<i className="fa fa-user-secret"></i>
+					<a href="#">Admin Management</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-building-o"></i>
+					<a href="#">Company Management</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-wrench"></i>
+					<a href="#">Employee Management</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-archive"></i>
+					<a href="#">Warehouse</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-handshake-o"></i>
+					<a href="#">Contracts</a>
+				</div>
+				<h2>LEAVE</h2>
+				<div className="sidebar__link">
+					<i className="fa fa-question"></i>
+					<a href="#">Requests</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-sign-out"></i>
+					<a href="#">Leave Policy</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-calendar-check-o"></i>
+					<a href="#">Special Days</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-files-o"></i>
+					<a href="#">Apply for leave</a>
+				</div>
+				<h2>PAYROLL</h2>
+				<div className="sidebar__link">
+					<i className="fa fa-money"></i>
+					<a href="#">Payroll</a>
+				</div>
+				<div className="sidebar__link">
+					<i className="fa fa-briefcase"></i>
+					<a href="#">Paygrade</a>
+				</div>
+				<div className="sidebar__logout">
+					<i className="fa fa-power-off"></i>
+					<a href="#">Log out</a>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Sidebar;
