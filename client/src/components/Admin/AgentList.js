922408b092696422063ba102a90fabf5f6412d0e
tunde <icepaigez@gmail.com>
2021-01-15 13:16:40 +0100

    created a stylesheet for agentlist page

--- client/src/components/Admin/AgentList.js
+++ client/src/components/Admin/AgentList.js
@@ -8,6 +8,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import "./styles/agentlist.css";
+
 
 const useStyles = makeStyles({
 	table: {
