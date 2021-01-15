5dfaf9636f62142f9afadae47b80ed1c80b898ba
tunde <icepaigez@gmail.com>
2021-01-15 06:58:30 +0100

    created a file for modal to add new agent

--- client/src/components/Admin/Agents.js
+++ client/src/components/Admin/Agents.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./styles/agents.css";
 
 const Agents = () => {
+
+	const addAgent = () => {
+		console.log('adding new agent')
+	}
+
 	return(
 		<div className="agents__container">
 			<div className="agents__top__row">
 				<div className="agents__top__heading">
 					Team
 				</div>
-				<div className="add__agent">
+				<div onClick={addAgent} className="add__agent">
 					<button>
 						<div>+ Add</div>
 					</button>
