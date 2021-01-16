b85e757edd871d8da7c90e9032f8f49de3bc7561
tunde <icepaigez@gmail.com>
2021-01-16 20:41:49 +0100

    fixed the billing page

--- client/src/components/Admin/Payment/PaymentPlans.js
+++ client/src/components/Admin/Payment/PaymentPlans.js
@@ -1,79 +1,155 @@
-import React from "react";
+import React, { Component } from "react";
 
-const PaymentPlans = () => {
-	const getPlan = e => {
-		e.stopPropagation()
-		let selectedPrice = e.currentTarget.textContent.slice(0, 3)
-		console.log(selectedPrice)
+class PaymentPlans extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			totalBill: 0,
+			selectedRadio: "monthly",
+			currentAgentEntered: 0
+		}
 	}
-	return(
-		<div className="expanded__downright">
-			<div className="payment__container">
-				<div className="payment__top">
-					<div className="payment__top_toptext">
-						<p>Your current plan: <strong>Free Trial</strong></p>
-					</div>
-					<div className="payment__top_bottomtext">
-						<p>Ends in 3 days. Subscribe now for full access!</p>
-					</div>
-				</div>	
-				<div className="payment__mid">
-					<div onClick={getPlan} id="monthPlan" className="payment__mid__card">
-						<div className="payment__mid__price">
-							<h1>$25</h1>
+
+	getCycle = e => {
+		this.setState({
+			selectedRadio: e.target.value
+		})
+	}
+
+	handleRadioClick = (e) => {
+		this.setState({
+			selectedRadio: e.target.value
+		}, () => {if (this.state.currentAgentEntered) {
+					let totalCost;
+					let currentEntry = Math.abs(Number(this.state.currentAgentEntered))
+					if (this.state.selectedRadio === 'monthly') {
+						totalCost = currentEntry * 25
+						if (!isNaN(totalCost)) {
+							this.setState({
+								totalBill: totalCost
+							})
+						}
+					} else {
+						totalCost = currentEntry * 21 * 12
+						if (!isNaN(totalCost)) {
+							this.setState({
+								totalBill: totalCost
+							})
+						}
+					}
+				}})
+	}
+
+	getInput = e => {
+		return e.target.value
+	}
+
+
+	getAgentInput = (e) => {
+		const { selectedRadio } = this.state
+		let agentCount = Math.abs(Number(e.target.value))
+		let totalCost;
+		if (selectedRadio === 'monthly') {
+			totalCost = agentCount * 25
+			if (!isNaN(totalCost)) {
+				this.setState({
+					totalBill: totalCost,
+					currentAgentEntered: agentCount
+				})
+			}
+		} else {
+			totalCost = agentCount * 21 * 12
+			if (!isNaN(totalCost)) {
+				this.setState({
+					totalBill: totalCost,
+					currentAgentEntered: agentCount
+				})
+			}
+		}
+	}
+	
+
+	render() {
+		const { totalBill, selectedRadio } = this.state
+			return(
+			<div className="expanded__downright">
+				<div className="payment__container">
+					<div className="payment__top">
+						<div className="payment__top_toptext">
+							<p>Your current plan: <strong>Free Trial</strong></p>
 						</div>
-						<div className="payment__mid__desc">
-							<p>per agent, per month</p>
-							<p>billed monthly</p>
+						<div className="payment__top_bottomtext">
+							<p>Ends in 3 days. Subscribe now for full access!</p>
+						</div>
+					</div>	
+					<div className="payment__mid">
+						<div className= "payment__mid__card">
+							<div className="payment__mid__price">
+								<h1>$25</h1>
+							</div>
+							<div className="payment__mid__desc">
+								<p>per agent, per month</p>
+								<p>billed monthly</p>
+							</div>
+						</div>
+						<div className="payment__mid__card">
+							<div className="payment__mid__price">
+								<h1>$21</h1>
+							</div>
+							<div className="payment__mid__desc">
+								<p>per agent, per month</p>
+								<p>billed yearly</p>
+							</div>
 						</div>
 					</div>
-					<div onClick={getPlan} id="yearPlan" className="payment__mid__card">
-						<div className="payment__mid__price">
-							<h1>$21</h1>
+					<div className="payment__bottom">
+						<div className="agent__count">
+							<div className="label__text1">
+								<label htmlFor="agent_number">Agents</label>
+							</div>
+							<input onChange={this.getAgentInput} type="text" name="agent_number" placeholder="E.g. 1"/>
 						</div>
-						<div className="payment__mid__desc">
-							<p>per agent, per month</p>
-							<p>billed yearly</p>
+						<div className="billing__cycle">
+							<div className="label__text2">
+								<p>Billing cycle</p>
+							</div>
+							<div className="billing__radios">
+								<label className="billing__radio" htmlFor="monthly">
+									<input onClick={this.handleRadioClick} onChange={this.getCycle} type="radio" name="billCycle" value="monthly" checked={selectedRadio === 'monthly'}/>
+									<span className="label-visible">
+										<span className="second__span">
+											Monthly
+										</span>
+									</span>	 
+								</label>
+								<label className="billing__radio" htmlFor="yearly">
+									<input onClick={this.handleRadioClick} onChange={this.getCycle} type="radio" name="billCycle" value="yearly" checked={selectedRadio === 'yearly'}/>
+									<span className="label-visible">
+										<span className="second__span">
+											Yearly
+										</span>
+									</span>	 
+								</label>
+							</div>
 						</div>
-					</div>
+						<div className="total__bill">
+							<div className="bill__estimate">
+								<p>Estimated Total</p>
+							</div>
+							<div className="bill__value">
+								<h3>{ `$${totalBill}` }</h3>
+							</div>
+						</div>
+						<div className="submit__payment__plan">
+							<button>Subscribe Now</button>
+						</div>
+					</div>	
 				</div>
-				<div className="payment__bottom">
-					<div className="agent__count">
-						<label htmlFor="agent_number">Agents</label>
-						<input type="text" name="agent_number" placeholder="E.g. 5"/>
-					</div>
-					<div className="billing__cycle">
-						<p>Billing cycle</p>
-						<div className="billing__radios">
-							<label className="billing__radio" htmlFor="monthly">
-								<input type="radio" name="billCycle" value="monthly" defaultChecked/>
-								<span className="label-visible">
-									<span className="second__span">
-										Monthly
-									</span>
-								</span>	 
-							</label>
-							<label className="billing__radio" htmlFor="yearly">
-								<input type="radio" name="billCycle" value="yearly"/>
-								<span className="label-visible">
-									<span className="second__span">
-										Yearly
-									</span>
-								</span>	 
-							</label>
-						</div>
-					</div>
-					<div className="total__bill">
-						<p>Estimated Total</p>
-						<h3>$400</h3>
-					</div>
-					<div className="submit__payment__plan">
-						<button>Subscribe Now</button>
-					</div>
-				</div>	
 			</div>
-		</div>
-	)
+		)
+	}
+	
 }
 
 export default PaymentPlans;
