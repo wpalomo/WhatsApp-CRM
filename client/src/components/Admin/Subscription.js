3ac08996b372219c1c3a43c3a0d59dd6b6c517b4
tunde <icepaigez@gmail.com>
2021-01-16 06:45:47 +0100

    styling the subscription page

--- client/src/components/Admin/Subscription.js
+++ client/src/components/Admin/Subscription.js
@@ -5,7 +5,18 @@ import "./styles/subscription.css";
 const Subscription = () => {
 	return(
 		<div className="subscr__container">
-			<h1>Pay</h1>
+			<div className="billing__heading">
+				<p>Billing</p>
+			</div>
+			<div className="billing__footer">
+				<h3>Bottom</h3>
+			</div>
+			<div className="expanded__topright">
+				<h3>RightTop</h3>
+			</div>
+			<div className="expanded__downright">
+				<h3>RightDown</h3>
+			</div>
 		</div>
 	)
 }
