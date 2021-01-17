b0988ea4dfb4130b6625b25b5b989880e445efcb
tunde <icepaigez@gmail.com>
2021-01-17 18:07:50 +0100

    fixed flutterwave phone number api field

--- client/src/components/Admin/Payment/CreditCardModal.js
+++ client/src/components/Admin/Payment/CreditCardModal.js
@@ -6,23 +6,23 @@ import "../styles/cardModal.css";
 const CreditCardModal = ({ totalBill, agents, getError }) => {
 	
 	let ftwKey = process.env.REACT_APP_FLUTTERWAVE_PUBLIC_KEY
-	
+
 	const config = {
-    public_key: ftwKey,
-    tx_ref: Date.now(),
-    amount: totalBill,
-    currency: 'USD',
-    payment_options: 'card,mobilemoney,ussd',
-    customer: {
-      email: 'oduguwaoa@gmail.com',
-      phonenumber: '07030117552',
-      name: 'Tunde Oduguwa',
-    },
-    customizations: {
-      title: 'Sauceflow WhatsApp CRM',
-      description: `${agents} Agents`,
-      logo: '',
-    },
+	    public_key: ftwKey,
+	    tx_ref: Date.now(),
+	    amount: totalBill,
+	    currency: 'USD',
+	    payment_options: 'card,mobilemoney,ussd',
+	    customer: {
+	      email: 'oduguwaoa@gmail.com',
+	      phone_number: '+2347030117552',
+	      name: 'Tunde Oduguwa',
+	    },
+	    customizations: {
+	      title: 'WhatsApp CRM',
+	      description: `${agents} Agents`,
+	      logo: '',
+	    },
   };
 
   const handleFlutterPayment = useFlutterwave(config);
