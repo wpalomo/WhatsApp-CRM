e1197828e17afad4c04b089c99dc4bd9e7e84f22
tunde <icepaigez@gmail.com>
2021-01-17 19:31:21 +0100

    set up styling for the modals

--- client/src/components/Admin/Payment/ErrorModal.js
+++ client/src/components/Admin/Payment/ErrorModal.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../styles/cardModal.css";
+
 
 const ErrorModal = ({ show }) => {
 	if (!show) {
--- client/src/components/Admin/Payment/SuccessModal.js
+++ client/src/components/Admin/Payment/SuccessModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/cardModal.css";
 
 const SuccessModal = ({ show }) => {
 	if (!show) {
