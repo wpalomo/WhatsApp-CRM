0bae77c75fc16600ce90f04c28a6bfc1802322d0
tunde <icepaigez@gmail.com>
2021-01-19 18:05:57 +0100

    setting up firebase auth methods

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Firebase, { FirebaseContext } from "./firebase/index";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<FirebaseContext.Provider value={new Firebase()}>
+    	<App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') 
 );
 
 // If you want to start measuring performance in your app, pass a function
