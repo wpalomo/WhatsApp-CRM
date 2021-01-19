2af6ecbc6c06951e74dfb6a5f1d1804de9520a6f
tunde <icepaigez@gmail.com>
2021-01-19 16:51:51 +0100

    pre styling of the home page

--- client/src/components/HomePage/Homepage.js
+++ client/src/components/HomePage/Homepage.js
@@ -1,8 +1,24 @@
 import React from "react";
+import "./homepage.css";
 
 const HomePage = () => {
 	return(
-		<h1>Welcome</h1>
+		<div className="homepage__container">
+			<div className="home__container">
+				<div className="home__header">
+					Header
+				</div>
+				<div className="home__menu">
+					Menu
+				</div>
+				<div className="home__content">
+					Content
+				</div>
+				<div className="home__footer">
+					Footer
+				</div>
+			</div>
+		</div>
 	)
 }
 
