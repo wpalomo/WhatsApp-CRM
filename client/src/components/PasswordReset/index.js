fdc6b38a5b7e42fa4628fbfc6143fc8bb48992ce
tunde <icepaigez@gmail.com>
2021-01-22 15:54:57 +0100

    fixed styling for password reset form

--- client/src/components/PasswordReset/index.js
+++ client/src/components/PasswordReset/index.js
@@ -69,11 +69,11 @@ class PasswordResetFormBase extends Component {
 		return(
 			<div className="register__container">  
 				<p id="brand__name">Sauceflow</p>
-				<div className="register__body">
+				<div className="register__body password__reset">
 					<div className="form__heading">
 						<p>Password Update</p>
 					</div>
-						<div className="form__container password__reset">
+						<div className="form__container">
 								<div className="password__container">
 									<label className="field__label" htmlFor="owner__password">New Password</label>
 									<div className="password__container__input">
