14b09897ada1613ad9cddd56a2edd4b61770bc76
tunde <icepaigez@gmail.com>
2021-01-31 13:10:16 +0100

    set up home page

--- client/src/components/HomePage/Homepage.js
+++ client/src/components/HomePage/Homepage.js
@@ -17,12 +17,28 @@ const HomePage = () => {
 		<div className="homepage__container">
 			<div className="home__container">
 				<div className="home__leftarea">
-					<div className="home__header">
-					<p onClick={getSigninPage}>Sign In</p>
-					<p onClick={getRegisterPage}>Sign Up</p>		
+					<div className="home__leftarea__top">
+						<h1>Sauceflow</h1>
+						<button onClick={getSigninPage}>Sign In</button>	
 					</div>
-					<div className="home__content">
-						Content
+					<div className="home__leftarea__bottom">
+						<div className="hero__text">
+							<h5>One Business | Multiple Agents </h5>
+							<h5>One WhatsApp Number</h5>
+						</div>
+						<div className="middle__hero__text">
+							<p>
+								Sauceflow allows all your customer support staff connect
+								with your customers without restriction to one phone or
+								one WhatsApp web session. 
+							</p>
+						</div>
+						<div className="hero__button">
+							<button onClick={getRegisterPage}>Sign Up</button>
+						</div>
+					</div>
+					<div className="home__leftarea__footer">
+						<a href="mailto:sauce@sauceflow.com">Contact us</a>
 					</div>
 				</div>
 				<div className="home__rightarea">
