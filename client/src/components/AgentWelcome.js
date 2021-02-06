a405f78e72262a5569b4db9294dec3a54985cb6f
tunde <icepaigez@gmail.com>
2021-02-06 12:43:26 +0100

    added animation file to the welcome page

--- client/src/components/AgentWelcome.js
+++ client/src/components/AgentWelcome.js
@@ -1,15 +1,19 @@
 import React from "react";
 import HomeAnimation from "./HomePage/HomeAnimation";
 import "./styles/welcome.css";
-import animationData from "../anime/team.json";
+import animationData from "../anime/welcome.json";
 
 const AgentWelcome = ({ name }) => {
 	let firstName = name.split(" ")[0]
 	return(
 		<div className="agent__welcome">
 			<div className="welcome__container">
-				<HomeAnimation animationData={animationData} style={{ width: 300, height: 300 }}/>
-				<p>Welcome { firstName }!</p>
+				<div className="welcome__anime">
+					<HomeAnimation animationData={animationData} style={{ width: 400, height: 400 }}/>
+				</div>
+				<div className="welcome__text">
+					<p>Welcome { firstName }!</p>
+				</div>
 			</div>
 		</div>
 	)
